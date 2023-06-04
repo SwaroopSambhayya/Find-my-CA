40852f93094dfdcc857f648c54c71de9342cb4ff
Ganesh Bhat <ganeshbhat015@gmail.com>
2023-06-04 17:14:00 +0530

    Implement shellroute

--- lib/features/client/home/home.dart
+++ lib/features/client/home/home.dart
@@ -1,5 +1,7 @@
 import 'package:find_my_ca/features/client/home/components/logout_button.dart';
+import 'package:find_my_ca/shared/providers/route_const.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -10,7 +12,13 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          actions: const [LogoutButton()],
+          actions: [
+            IconButton(
+                onPressed: () {
+                  context.go('/home/profile');
+                },
+                icon: const Icon(Icons.person))
+          ],
         ),
         body: const Placeholder());
   }
--- lib/shared/providers/router_provider.dart
+++ lib/shared/providers/router_provider.dart
@@ -2,6 +2,7 @@ import 'package:appwrite/models.dart';
 import 'package:find_my_ca/features/auth/auth.dart';
 import 'package:find_my_ca/features/auth/login/login_provider.dart';
 import 'package:find_my_ca/features/client/home/home.dart';
+import 'package:find_my_ca/features/profile/profile.dart';
 import 'package:find_my_ca/shared/providers/appwrite_realtime.dart';
 import 'package:find_my_ca/shared/providers/route_const.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 final _key = GlobalKey<NavigatorState>();
+final _shellKey = GlobalKey<NavigatorState>();
 
 final routeProvider = Provider<GoRouter>((ref) {
   final authState = ref.watch(authStateProvider);
@@ -18,13 +20,28 @@ final routeProvider = Provider<GoRouter>((ref) {
     navigatorKey: _key,
     debugLogDiagnostics: true,
     routes: [
-      GoRoute(
-          path: '/',
-          name: home,
-          builder: (context, state) {
-            return const Home();
-          },
-          routes: [GoRoute(path: '/$profile', name: profile)]),
+      ShellRoute(
+        navigatorKey: _shellKey,
+        routes: [
+          GoRoute(
+            path: '/',
+            name: home,
+            builder: (context, state) {
+              return const Home();
+            },
+          ),
+          GoRoute(
+            path: '/home/profile',
+            name: profile,
+            builder: (context, state) {
+              return const Profile();
+            },
+          ),
+        ],
+        builder: (context, state, child) {
+          return child;
+        },
+      ),
       GoRoute(
           path: login,
           name: login,

a3132f9c4a6037d7e8d188a6ae5ae9b4a2b653ba
Ganesh Bhat <ganeshbhat015@gmail.com>
2023-06-05 22:11:57 +0530

    Navigation issue fixes (#8)
    Co-authored-by: Swaroop Sambhayya <swaroop.sambhayya@gmail.com>

--- lib/features/auth/login/login_provider.dart
+++ lib/features/auth/login/login_provider.dart
@@ -2,14 +2,19 @@ import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart';
 import 'package:find_my_ca/features/auth/login/login_repositoty.dart';
 import 'package:find_my_ca/shared/providers/account_provider.dart';
+import 'package:find_my_ca/shared/providers/appwrite_realtime.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final loginProvider = StateNotifierProvider<LoginAuth, AsyncValue<void>>((ref) {
   Account account = ref.read(accountProvider);
-  return LoginAuth(account);
+  return LoginAuth(account, ref);
 });
 
 final authStateProvider = FutureProvider<User?>((ref) async {
   User? user = await ref.read(accountProvider).get();
+  // ignore: unnecessary_type_check
+  if (user is User) {
+    ref.read(authStateListener.notifier).state = ['users.*.sessions.*.create'];
+  }
   return user;
 });

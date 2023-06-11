378f68b2c9341e5b0661e88455b3b392dced7142
Paritosh <paritosh209@gmail.com>
2023-06-11 18:41:42 +0530

    getCAList data added

--- lib/features/client/home/home.dart
+++ lib/features/client/home/home.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:find_my_ca/features/client/home/providers/provider.dart';
+import 'package:find_my_ca/features/profile/providers/profile_provider.dart';
 import 'package:find_my_ca/shared/providers/route_const.dart';
 import 'package:find_my_ca/shared/theme.dart';
 import 'package:flutter/material.dart';
@@ -6,14 +8,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../shared/const.dart';
+import '../../../shared/models/profile.dart';
 import '../../../shared/providers/account_provider.dart';
 
 class Home extends ConsumerStatefulWidget {
   const Home({super.key});
-
-  static String get routeName => 'home';
-
-  static String get routeLocation => '/';
 
   @override
   ConsumerState<Home> createState() => _HomeState();
@@ -21,73 +20,46 @@ class Home extends ConsumerStatefulWidget {
 
 class _HomeState extends ConsumerState<Home> {
   @override
-  void initState() {
-    super.initState();
-    // getUserProfile();
-  }
-
-  // getUserProfile() async {
-  //   final account = ref.read(accountProvider);
-  //   final user = await account.get();
-  //   final userId = user.$id;
-  //   final database = ref.read(databaseProvider);
-  //   final profileData = await database.getDocument(
-  //       databaseId: databaseId,
-  //       collectionId: profileCollectionID,
-  //       documentId: userId);
-  //   final profile = Profile.fromMap(profileData.data);
-  //   setState(() {
-  //     userProfile = profile;
-  //     isProfileLoaded = true;
-  //   });
-  //
-  // }
-
-
-  @override
   Widget build(BuildContext context) {
-    final home = ref.watch(currentHomeProvider);
-
-    return home.when(
-        loading: () => const CircularProgressIndicator(),
-        error: (err, stack) => Text('Error: $err'),
-        data: (data) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                "Home",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-              ),
-              centerTitle: true,
-              backgroundColor: backgroundColor,
-            ),
-            body: SafeArea(
-              child: ListView(
-                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                children: [
-                  const Text("Hi, Gunther",
-                      style:
-                          TextStyle(fontSize: 14, color: Colors.grey, fontWeight: FontWeight.w500)),
-                  Text(homeHeading,
-                      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
-                  const SizedBox(height: 30),
-                  const Text("Categories",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
-                  const CategoryItems(),
-                  const Text("Top CA", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
-                  const SizedBox(height: 5),
-                  // home.when(
-                  //     loading: () => const CircularProgressIndicator(),
-                  //     error: (err, stack) => Text('Error: $err'),
-                  //     data: (data) {
-                  //       return List.generate(5, (index) => const CaListItem());
-                  //     })
-                  ...List.generate(5, (index) => const CaListItem())
-                ],
-              ),
-            ),
-          );
-        });
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Home",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+        centerTitle: true,
+        backgroundColor: backgroundColor,
+      ),
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          children: [
+            ref.watch(profileProvider).when(
+                loading: () => const Center(child: CircularProgressIndicator()),
+                error: (err, stack) => Text('Error: $err'),
+                data: (data) {
+                  return Text("Hi, ${data?.fname ?? ''}",
+                      style: const TextStyle(
+                          fontSize: 14, color: Colors.grey, fontWeight: FontWeight.w500));
+                }),
+            Text(homeHeading, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+            const SizedBox(height: 30),
+            const Text("Categories", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+            const CategoryItems(),
+            const Text("Top CA", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+            const SizedBox(height: 5),
+            ref.watch(currentHomeProvider).when(
+                loading: () => const Center(child: CircularProgressIndicator()),
+                error: (err, stack) => Text('Error: $err'),
+                data: (data) {
+                  return Column(
+                      children: List.generate(
+                          data.length, (i) => CaListItem(profileData: data, index: i)));
+                })
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -100,8 +72,6 @@ class CategoryItems extends StatefulWidget {
 
 class _CategoryItemsState extends State<CategoryItems> {
   int selectedIndex = 0;
-
-  // final List<String> _choicesList = ['General', 'Criminal', 'Crime', "Tax Deduction"];
 
   @override
   Widget build(BuildContext context) {
@@ -164,11 +134,14 @@ class _CategoryItemsState extends State<CategoryItems> {
 }
 
 class CaListItem extends StatelessWidget {
-  const CaListItem({Key? key}) : super(key: key);
+  final List<Profile>? profileData;
+  final int? index;
+
+  const CaListItem({Key? key, this.profileData, this.index}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
+    // print("check data: ${profileData?.first.fname}");
     return Container(
       height: 120,
       margin: const EdgeInsets.symmetric(vertical: 5),
@@ -185,11 +158,14 @@ class CaListItem extends StatelessWidget {
                 width: 100,
                 height: 100,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  image: const DecorationImage(
-                      image: NetworkImage(
-                          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"),
-                      fit: BoxFit.fill),
+                  borderRadius: BorderRadius.circular(12)
+                ),
+                child: CachedNetworkImage(
+                  imageUrl:
+                      "$appWriteBaseURl/storage/buckets/$profilePicBucketId/files/${profileData?[index!].userId}/preview?project=$projectID",
+                  fit: BoxFit.fill,
+                  placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
+                  errorWidget: (context, url, error) => const Center(child: Icon(Icons.error)),
                 ),
               ),
               const SizedBox(width: 10),
@@ -203,19 +179,21 @@ class CaListItem extends StatelessWidget {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          const Text("Richard Tea", style: TextStyle(fontSize: 12)),
+                          Text(profileData?[index!].fname ?? '',
+                              style: const TextStyle(fontSize: 12)),
                           Container(
                             padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(50),
                               color: primaryColor.withOpacity(0.5),
                             ),
-                            child: const Text("General", style: TextStyle(fontSize: 12)),
+                            child: Text(profileData?[index!].expertise?.first ?? '',
+                                style: TextStyle(fontSize: 12)),
                           )
                         ],
                       ),
-                      const Text("Binghamton, New York",
-                          style: TextStyle(fontSize: 12, color: Colors.grey)),
+                      Text(profileData?[index!].address ?? '',
+                          style: const TextStyle(fontSize: 12, color: Colors.grey)),
                       const Row(
                         children: [
                           Icon(Icons.star_rate, color: Colors.yellow, size: 16),

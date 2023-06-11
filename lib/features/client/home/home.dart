78d2158afefe8fb5f664e173edcadf8cf3b74f15
Ganesh Bhat <ganeshbhat015@gmail.com>
2023-06-11 20:13:57 +0530

    Implement fcm initialisation

--- lib/features/client/home/home.dart
+++ lib/features/client/home/home.dart
@@ -1,15 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:find_my_ca/features/client/home/providers/provider.dart';
 import 'package:find_my_ca/features/profile/providers/profile_provider.dart';
-import 'package:find_my_ca/shared/providers/route_const.dart';
+import 'package:find_my_ca/shared/services.dart';
 import 'package:find_my_ca/shared/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../../shared/const.dart';
 import '../../../shared/models/profile.dart';
-import '../../../shared/providers/account_provider.dart';
 
 class Home extends ConsumerStatefulWidget {
   const Home({super.key});
@@ -20,16 +18,16 @@ class Home extends ConsumerStatefulWidget {
 
 class _HomeState extends ConsumerState<Home> {
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      initFirebaseMessaging(context);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Home",
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-        ),
-        centerTitle: true,
-        backgroundColor: backgroundColor,
-      ),
       body: SafeArea(
         child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
@@ -40,22 +38,34 @@ class _HomeState extends ConsumerState<Home> {
                 data: (data) {
                   return Text("Hi, ${data?.fname ?? ''}",
                       style: const TextStyle(
-                          fontSize: 14, color: Colors.grey, fontWeight: FontWeight.w500));
+                          fontSize: 14,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w500));
                 }),
-            Text(homeHeading, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
-            const SizedBox(height: 30),
-            const Text("Categories", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+            Text(homeHeading,
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+            const SizedBox(height: 24),
+            const Text("Select Categories",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
             const CategoryItems(),
-            const Text("Top CA", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+            const SizedBox(height: 8),
+            const Text("Top CA",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
             const SizedBox(height: 5),
             ref.watch(currentHomeProvider).when(
-                loading: () => const Center(child: CircularProgressIndicator()),
-                error: (err, stack) => Text('Error: $err'),
-                data: (data) {
-                  return Column(
+                  loading: () =>
+                      const Center(child: CircularProgressIndicator()),
+                  error: (err, stack) => Text('Error: $err'),
+                  data: (data) {
+                    return Column(
                       children: List.generate(
-                          data.length, (i) => CaListItem(profileData: data, index: i)));
-                })
+                        data.length,
+                        (i) => CaListItem(profileData: data, index: i),
+                      ),
+                    );
+                  },
+                )
           ],
         ),
       ),
@@ -99,14 +109,16 @@ class _CategoryItemsState extends State<CategoryItems> {
                                 height: 25,
                                 width: 25,
                                 decoration: BoxDecoration(
-                                    color: backgroundColor, borderRadius: BorderRadius.circular(5)),
-                                child:
-                                    Icon(Icons.add_business, size: 18, color: getIconColor(index)),
+                                    color: backgroundColor,
+                                    borderRadius: BorderRadius.circular(5)),
+                                child: Icon(Icons.add_business,
+                                    size: 18, color: getIconColor(index)),
                               ),
                               const SizedBox(width: 10),
                               Text(
                                 expertise[index],
-                                style: TextStyle(color: getFontColor(index), fontSize: 14),
+                                style: TextStyle(
+                                    color: getFontColor(index), fontSize: 14),
                               ),
                             ],
                           ),
@@ -141,9 +153,11 @@ class CaListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // print("check data: ${profileData?.first.fname}");
+    final imageUrl =
+        "$appWriteBaseURl/storage/buckets/$profilePicBucketId/files/${profileData?[index!].id}/preview?project=$projectID";
+
     return Container(
-      height: 120,
+      height: 132,
       margin: const EdgeInsets.symmetric(vertical: 5),
       child: Card(
         elevation: 2,
@@ -154,53 +168,96 @@ class CaListItem extends StatelessWidget {
           padding: const EdgeInsets.all(10),
           child: Row(
             children: [
-              Container(
+              SizedBox(
                 width: 100,
                 height: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12)
-                ),
-                child: CachedNetworkImage(
-                  imageUrl:
-                      "$appWriteBaseURl/storage/buckets/$profilePicBucketId/files/${profileData?[index!].userId}/preview?project=$projectID",
-                  fit: BoxFit.fill,
-                  placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
-                  errorWidget: (context, url, error) => const Center(child: Icon(Icons.error)),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                  child: CachedNetworkImage(
+                    imageUrl: imageUrl,
+                    fit: BoxFit.cover,
+                    placeholder: (context, url) =>
+                        const Center(child: CircularProgressIndicator()),
+                    errorWidget: (context, url, error) => const SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: Center(
+                            child: Icon(
+                          Icons.person,
+                          size: 42,
+                        ))),
+                  ),
                 ),
               ),
               const SizedBox(width: 10),
               Expanded(
                 child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  padding: const EdgeInsets.all(4),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(profileData?[index!].fname ?? '',
-                              style: const TextStyle(fontSize: 12)),
-                          Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              color: primaryColor.withOpacity(0.5),
-                            ),
-                            child: Text(profileData?[index!].expertise?.first ?? '',
-                                style: TextStyle(fontSize: 12)),
+                          Text(
+                            " ${profileData?[index!].fname} ${profileData?[index!].lname}",
+                          ),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(Icons.star_rate,
+                                  color: primaryColor, size: 20),
+                              const SizedBox(width: 2),
+                              //TODO:
+                              const Text("4.8",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    // color: Colors.grey,
+                                  ))
+                            ],
                           )
                         ],
                       ),
-                      Text(profileData?[index!].address ?? '',
-                          style: const TextStyle(fontSize: 12, color: Colors.grey)),
-                      const Row(
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                            size: 16,
+                            color: Colors.grey[500]!,
+                          ),
+                          Text(
+                            profileData?[index!].city ?? '',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey[500]!,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        profileData?[index!].profileDescription ?? '',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          // color: Colors.grey,
+                        ),
+                      ),
+                      /* const Row(
                         children: [
                           Icon(Icons.star_rate, color: Colors.yellow, size: 16),
                           SizedBox(width: 5),
-                          Text("4.8", style: TextStyle(fontSize: 10, color: Colors.grey))
+                          Text("4.8",
+                              style:
+                                  TextStyle(fontSize: 10, color: Colors.grey))
                         ],
-                      )
+                      ) */
                     ],
                   ),
                 ),
